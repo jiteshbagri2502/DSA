12c2a809a69b22e95ab4ebc888253c9aa7868139
jiteshbagri2502 <jiteshbagri2502@gmail.com>
2026-07-14 21:04:32 +0530

    Time: 28 ms (49.31%) | Memory: 17.5 MB (43.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
 
-    void bfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int row, int col){
+    void dfs(vector<vector<char>>& grid,vector<vector<int>>& vis, int row, int col){
         int m = grid.size();
         int n = grid[0].size();
-        queue<pair<int,int>> q;
-        q.push({row,col});
         vis[row][col] = 1;
-
         int dRow[] = {-1,0,1,0};
         int dCol[] = {0,1,0,-1};
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-            for(int i = 0; i < 4; i++){
-                int nrow = row + dRow[i];
-                int ncol = col + dCol[i];
-                if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
-                    q.push({nrow,ncol});
-                    vis[nrow][ncol] = 1;
-                }
+        for(int i = 0; i < 4; i++){
+            int nrow = row + dRow[i];
+            int ncol = col + dCol[i];
+            if(nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && !vis[nrow][ncol] && grid[nrow][ncol] == '1'){
+                dfs(grid,vis,nrow,ncol);
             }
         }
-        
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> vis(m, vector<int> (n,0));
+        vector<vector<int>> vis(m, vector<int>(n,0));
         int count = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(!vis[i][j] && grid[i][j] == '1'){
                     count++;
-                    bfs(grid, vis, i, j);
+                    dfs(grid,vis,i,j);
                 }
             }
         }
