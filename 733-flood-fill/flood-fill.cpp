ae68fd0350ce9b693e2812c38afb31d309e1caa4
jiteshbagri2502 <jiteshbagri2502@gmail.com>
2026-07-14 23:30:07 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (32.29%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc] == color){return image;}
         int m = image.size();
         int n = image[0].size();
         int old = image[sr][sc];
+        if(color==old){
+            return image;
+        }
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
         image[sr][sc] = color;
         int dr[] = {-1,0,1,0};
         int dc[] = {0,1,0,-1};
-        for(int i = 0 ; i < 4; i++){
-            int nr = sr + dr[i];
-            int nc = sc + dc[i];
-            if(nr >= 0 && nc >= 0 && nr < m && nc < n && image[nr][nc] == old){
-                floodFill(image,nr,nc,color);
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            for(int i = 0; i < 4; i++){
+                int nr = row + dr[i];
+                int nc = col + dc[i];
+                if(nr >= 0 && nc >= 0 && nr < m && nc < n && image[nr][nc] == old){
+                    q.push({nr,nc});
+                    image[nr][nc] = color;
+                }
+            
             }
         }
         return image;
