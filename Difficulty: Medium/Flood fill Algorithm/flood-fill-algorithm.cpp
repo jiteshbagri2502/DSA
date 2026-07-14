ad7d5644fdb32cc8f86b0b212bc483c5709e0960
jiteshbagri2502 <jiteshbagri2502@gmail.com>
2026-07-14 23:23:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
+++ Difficulty: Medium/Flood fill Algorithm/flood-fill-algorithm.cpp
@@ -3,31 +3,21 @@ class Solution {
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
                                   int newColor) {
         // code here
+        if(image[sr][sc] == newColor){return image;}
         int m = image.size();
         int n = image[0].size();
         int old = image[sr][sc];
-        if(newColor==old){
-            return image;
-        }
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
         image[sr][sc] = newColor;
         int dr[] = {-1,0,1,0};
         int dc[] = {0,1,0,-1};
-        while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
-            q.pop();
-            for(int i = 0; i < 4; i++){
-                int nr = row + dr[i];
-                int nc = col + dc[i];
-                if(nr >= 0 && nc >= 0 && nr < m && nc < n && image[nr][nc] == old){
-                    q.push({nr,nc});
-                    image[nr][nc] = newColor;
-                }
-            
+        for(int i = 0 ; i < 4; i++){
+            int nr = sr + dr[i];
+            int nc = sc + dc[i];
+            if(nr >= 0 && nc >= 0 && nr < m && nc < n && image[nr][nc] == old){
+                floodFill(image,nr,nc,newColor);
             }
         }
         return image;
+        
     }
 };
