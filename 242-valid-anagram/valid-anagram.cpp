f861b7120a7d228d6b7f02a9ea64141c0d124f43
jiteshbagri2502 <jiteshbagri2502@gmail.com>
2026-07-26 10:57:58 +0530

    Time: 1 ms (54.95%) | Memory: 9.6 MB (92.97%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int count[26] = {0};
+        for(auto it: s){
+            count[it-'a']++;
+        }
+        for(auto it: t){
+            count[it-'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(count[i] != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
