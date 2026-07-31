c69d663a8cbd9163d2fa8a88c79bffc8e22e9ece
jiteshbagri2502 <jiteshbagri2502@gmail.com>
2026-07-31 19:45:59 +0530

    Time: 5 ms (18.08%) | Memory: 312.2 MB (16.56%) - LeetSync

--- 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/delete-the-middle-node-of-a-linked-list.cpp
@@ -15,12 +15,13 @@ public:
             return NULL;
         }
         ListNode* slow = head;
-        ListNode* fast = slow->next->next;
+        ListNode* fast = head->next->next;
         while(fast != NULL && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
         }
         slow->next = slow->next->next;
         return head;
+
     }
 };
